c6a7afd1d9682ca4a52e7eab7b3c94d66e7ee1a1
Кирилл Фунтов <funtaps@gmail.com>
2021-07-30 00:41:43 +0300

    Nginx + php-fpm

---
+++ system/admin/index.php
@@ -0,0 +1,13 @@
+<h1>Админка</h1>
+<hr/>
+<img src="./logo.jpg" />
+<hr/>
+<?php
+
+echo rand(0, 100);
+?>
+<hr/>
+<form method="POST">
+    <input type="text" name="o_id" placeholder="Номер заказа"/>
+    <input type ="submit" value="Найти"/>
+</form>
