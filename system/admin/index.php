27330f5b5f22baf9885294b23732f052b1b9255e
Кирилл Фунтов <funtaps@gmail.com>
2021-08-06 14:17:06 +0300

    Ansible configs

--- system/admin/index.php
+++ system/admin/index.php
@@ -7,7 +7,7 @@
 echo rand(0, 100);
 ?>
 <hr/>
-<form method="POST">
+<form method="POST" action="order.php">
     <input type="text" name="o_id" placeholder="Номер заказа"/>
     <input type ="submit" value="Найти"/>
 </form>
